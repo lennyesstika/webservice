720035a2ec3542ee16ad39cd2bb1a148c429e050
lennyesstika <43899877+lennyesstika@users.noreply.github.com>
2019-03-14 19:13:08 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Web Service</title>
+</head>
+<body>
+    <p>
+    <?php echo 'HELLO WORLD!'; ?> <br>
+    <?php echo 'Nama : Lenny Estika Sari'; ?> <br>
+    <?php echo 'Nim : 16.01.53.0137!'; ?> <br>
+    </p>
+    
+</body>
+</html>
